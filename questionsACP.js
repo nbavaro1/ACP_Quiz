089e8e5ef7b20fdd491bc67e78fd3d37b1afc5fd
nbavaro1 <39228701+nbavaro1@users.noreply.github.com>
2021-01-30 19:43:54 -0500

    Update questionsACP.js

--- questionsACP.js
+++ questionsACP.js
@@ -165,6 +165,158 @@ var questions = [
         "option2": "Unit testing",
         "option3": "Iteration demos",
         "option4": "Iteration planning",
+        "answer": 4
+    },
+    {
+        "question": "Which set of tools is least likely to be used by an agile team?",
+        "option1": "Digital camera, task board",
+        "option2": "Stickieson a whiteboard, user story cards",
+        "option3": "Scheduling software, Gantt charts",
+        "option4": "Smart board, card wall",
         "answer": 3
+    },
+    {
+        "question": "A sponsor wants to evaluate a proposed three-year project against two proposed one-year projects. Which financial metric would be most helpful?",
+        "option1": "NPV",
+        "option2": "ROI",
+        "option3": "MMF",
+        "option4": "Velocity",
+        "answer": 1
+    },
+    {
+        "question": "The purpose of continuous integration is to:",
+        "option1": "Always have a version of the software ready for release",
+        "option2": "Shorten lengthy compile times",
+        "option3": "Facilitate the practice of pair programming",
+        "option4": "Find code issues as soon as possible",
+        "answer": 4
+    },
+    {
+        "question": "Which of the following is NOT a recognized agile prioritization scheme?",
+        "option1": "Prioritization based on business value",
+        "option2": "Prioritization based on reducing risk",
+        "option3": "Prioritization based on iteration velocity",
+        "option4": "Prioritization based on business value and risk",
+        "answer": 3
+    },
+    {
+        "question": "Which of the following is true about agile contracts?",
+        "option1": "They only work when the specs are fully defined.",
+        "option2": "The only work for time and materials agreements.",
+        "option3": "They need to be able to accommodate changes.",
+        "option4": "The cannot easily accommodate changes.",
+        "answer": 3
+    },
+    {
+        "question": "You are working on a software development team that follows a test-driven development process. The sequence of activities you would undertake is:",
+        "option1": "Write code, write test, refactor",
+        "option2": "Write test, refactor, write code",
+        "option3": "Write test, write code, refactor",
+        "option4": "Write code, refactor, write test",
+        "answer": 3
+    },
+    {
+        "question": "Which of the following is mostly likely to be an MVP?",
+        "option1": "A bicycle that has no handlebars",
+        "option2": "A pen that has  reservoir for glow-in-the-dark ink as well as regular ink",
+        "option3": "An order entry system that has no user interface",
+        "option4": "A chair that has no back",
+        "answer": 4
+    },
+    {
+        "question": "The sponsor is trying to determine which project has the greatest business value –one that returns $4 million in three years or one that returns $5 million in four  years.  The interest rate is 5 percent for borrowing capital to develop the projects.  The best approach to determine the highest value project is:",
+        "option1": "Select the “$5 million in four years” project, since it returns the highest amount",
+        "option2": "Select the “$4 million in three years” project since it has the shorter payback period",
+        "option3": "Calculate the NPV of the projects and choose the project with the highest value",
+        "option4": "Calculate the NPV of the projects and choose the project with the lowest value",
+        "answer": 3
+    },
+    {
+        "question": "You have been asked to outline the basics of agile contracting for your steering committee. Which of the following statements best describes the recommended approach to contracting on agile projects?",
+        "option1": "The contract is worded to allow for early completion of scope, and acceptance is based on the items matching the original specification.",
+        "option2": "The contract is worded to allow for reprioritization of scope, and acceptance is based on items matching the original specification.",
+        "option3": "The contract is worded to allow for early completion of scope, and acceptance is based on items being fit for business purpose.",
+        "option4": "The contract is worded to allow for reprioritization of scope, and acceptance is based on items being fit for business purpose.",
+        "answer": 4
+    },
+    {
+        "question": "MoSCoW and dot voting are forms of:",
+        "option1": "Estimation",
+        "option2": "Risk identification",
+        "option3": "Prioritization",
+        "option4": "Reporting",
+        "answer": 3
+    },
+    {
+        "question": "What is the primary benefit of involving business representatives in the prioritization of work?",
+        "option1": "To proactively engage the business representative between acceptance testing cycles",
+        "option2": "To answer business-related questions about the requirements",
+        "option3": "To promote communication about when features will be delivered",
+        "option4": "To better understand the business needs for the project",
+        "answer": 4
+    },
+    {
+        "question": "Which of the following is not a valid agile KPI?",
+        "option1": "Remaining work",
+        "option2": "Rate of progress",
+        "option3": "Rate of velocity",
+        "option4": "Likely completion date",
+        "answer": 3
+    },
+    {
+        "question": "Project X has an IRR of 12 percent, and project Y has an IRR of 10 percent. Which project represents the better rate of return?",
+        "option1": "It depends on the payback period.",
+        "option2": "Project Y",
+        "option3": "Project X",
+        "option4": "Project X or Y, depending on the NPV",
+        "answer": 3
+    },
+    {
+        "question": "Which of the statements below best describes the agile concept of incremental delivery?",
+        "option1": "The product is built and evaluated in chunks that are prioritized and selected by the development team.",
+        "option2": "The product is built and evaluated in chunks that, by the end of the project, comprise the complete product, including agreed-upon change requests that arose during the process.",
+        "option3": "The product is built and evaluated in chunks that, by the end of the project, comprise the complete product, excluding agreed-upon change requests that arose during the process.",
+        "option4": "The product is built and evaluated in agreed-upon increments that, by the end of the project, comprise the complete product, factoring in the support demand that will be required after delivery.",
+        "answer": 2
+    },
+    {
+        "question": "In software development, the process of frequently integrating new and changed code into the project code repository is known as:",
+        "option1": "Integrating daily",
+        "option2": "Continuous integration",
+        "option3": "Integration control ",
+        "option4": "Constant feedback",
+        "answer": 2
+    },
+    {
+        "question": "Little’s Law demonstrates that:",
+        "option1": "The duration of a work queue is dependent on its size.",
+        "option2": "The duration of a work queue allows us to predict how long the project will take.",
+        "option3": "WIP is equal to the duration of the work queue.",
+        "option4": "Cycle time is equal to the size of the work queue.",
+        "answer": 1
+    },
+    {
+        "question": "How is risk related to value delivery?",
+        "option1": "Risk is an inherent part of delivering value.",
+        "option2": "Risk increases as value decreases.",
+        "option3": "Risk is factored into value.",
+        "option4": "Risk is anti-value.",
+        "answer": 4
+    },
+    {
+        "question": "The shorthand summary for the test-driven development sequence is:",
+        "option1": "Red, Green, Yellow",
+        "option2": "Red, Green, Refactor",
+        "option3": "Green, Red, Clean",
+        "option4": "Green, Red, Refactor",
+        "answer": 2
+    },
+    {
+        "question": "What is an S-curve?",
+        "option1": "A graph that tracks costs or some other variable over time",
+        "option2": "An earned value management diagram",
+        "option3": "A sophisticated scheduling tool",
+        "option4": "A Gantt chart of spending over time",
+        "answer": 1
     }
 ];
